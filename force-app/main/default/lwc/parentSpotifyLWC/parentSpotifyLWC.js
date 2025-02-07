4db61e48bffc92cee3ab2c96f27ae61fba556167
ALAMURU SAINATH REDDY <alamurusainath345reddy@gmail.com>
2025-02-07 21:40:03 +0530

    JS Change

--- force-app/main/default/lwc/parentSpotifyLWC/parentSpotifyLWC.js
+++ force-app/main/default/lwc/parentSpotifyLWC/parentSpotifyLWC.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
+import noHeader from '@salesforce/resourceUrl/HeaderHide';
+import {loadStyle} from "lightning/platformResourceLoader";
 
-export default class ParentSpotifyLWC extends LightningElement {}
+export default class ParentSpotifyLWC extends LightningElement {
+    connectedCallback() {
+        loadStyle(this, noHeader)
+            .then(result => {});
+    }
+}
